04ac3271fc99852da61a771b13282a0161fe45dd
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-30 22:50:12 +0300

    refactor: defined styles in component

--- src/carousel/Slide.js
+++ src/carousel/Slide.js
@@ -5,12 +5,30 @@ import { css, jsx } from '@emotion/react';
 
 const Slide = (props) => {
 
-    const { slideData, slideStyles } = props;
+    const { slideData } = props;
+
+    const slideStyles = css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        padding: 0 1rem;
+        text-align: center;
+        background-color: #5b82a3;
+        pointer-events: none;
+        
+        & img {
+            height: 50%;
+            width: 50%;
+            object-fit: contain;
+        }
+    `;
 
     return (
         <div
             css={slideStyles}
-            className={'slide'}
         >
             <h2>{slideData.title}</h2>
             <p>{slideData.description}</p>
