42d5753f76d856ae71c934096a1f0b0dfefa050b
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-30 23:10:41 +0300

    refactor: remove test images

--- src/App.js
+++ src/App.js
@@ -2,39 +2,32 @@ import React from 'react';
 import Carousel from "./carousel/Carousel";
 import { css, jsx } from "@emotion/react";
 
-import capybara_1 from './img/capybara-1.png';
-import capybara_2 from './img/capybara-2.png';
-import capybara_3 from './img/capybara-3.png';
-import capybara_4 from './img/capybara-4.png';
-import capybara_5 from './img/capybara-5.png';
-
 /** @jsx jsx */
 
 const slidesData = [
     {
         title: 'Slide 1',
         description: 'Lorem ipsum sut dolor atmet and whatever comes after that',
-        img: capybara_1,
     },
+
     {
         title: 'Slide 2',
         description: 'Lorem ipsum sut dolor atmet and whatever comes after that',
-        img: capybara_2,
     },
+
     {
         title: 'Slide 3',
         description: 'Lorem ipsum sut dolor atmet and whatever comes after that',
-        img: capybara_3,
     },
+
     {
         title: 'Slide 4',
         description: 'Lorem ipsum sut dolor atmet and whatever comes after that',
-        img: capybara_4,
     },
+
     {
         title: 'Slide 5',
         description: 'Lorem ipsum sut dolor atmet and whatever comes after that',
-        img: capybara_5,
     },
 ]
 
